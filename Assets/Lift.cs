6a6a355a6c1ab30e1efdb302f6f8421b6cc02219
angel <angel@cyberwings.cyberywings>
2025-12-09 23:42:54 -0800

    Transitioned the enactment of force to rigidbodys

--- Assets/Lift.cs
+++ Assets/Lift.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using MathNet.Numerics;
 
 public class Lift : MonoBehaviour
 {
     private GameObject player;
-    private PhysicsBody playerPhysics;
+    private Rigidbody playerPhysics;
     [SerializeField] private float dragCoefficient = 1f;
     [SerializeField] private float area = 60f;
     [SerializeField] private float fluidDensity = 0.01f;
@@ -17,7 +18,7 @@ public class Lift : MonoBehaviour
     {
 
         player = GameObject.Find("Player");
-        playerPhysics = player.GetComponent<PhysicsBody>();
+        playerPhysics = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -32,18 +33,18 @@ public class Lift : MonoBehaviour
     {
 
         //drag direction/projected normal
-        Debug.DrawRay(transform.position, dragDirection * Vector3.Dot(transform.forward, playerPhysics.velocity.normalized) * Vector3.Normalize(playerPhysics.velocity), Color.green);
+        Debug.DrawRay(transform.position, dragDirection * Vector3.Dot(transform.forward, playerPhysics.linearVelocity.normalized) * Vector3.Normalize(playerPhysics.linearVelocity), Color.green);
 
         //lift
-        Debug.DrawRay(transform.position, liftDirection, Color.blue);
+        //Debug.DrawRay(transform.position, liftDirection, Color.blue);
     }
     void applyDrag()
     {
 
 
         Vector3 normal = transform.forward;
-        float projection = Vector3.Dot(normal, playerPhysics.velocity.normalized);
-        float Force = (0.5f * Mathf.Pow(Vector3.Magnitude(playerPhysics.velocity), 2) * area * fluidDensity * dragCoefficient);
+        float projection = Vector3.Dot(normal, playerPhysics.linearVelocity.normalized);
+        float Force = (0.5f * Mathf.Pow(Vector3.Magnitude(playerPhysics.linearVelocity), 2) * area * fluidDensity * dragCoefficient);
         if(projection > 0){
 
             dragDirection = -1;
@@ -55,25 +56,37 @@ public class Lift : MonoBehaviour
             dragDirection = 0;
         }
 
-        playerPhysics.netForce += (dragDirection * projection * Force * Vector3.Normalize(playerPhysics.velocity));
-        }
+        playerPhysics.AddForce(dragDirection * projection * Force * Vector3.Normalize(playerPhysics.linearVelocity));
+        applyTorque(dragDirection * projection * Force * Vector3.Normalize(playerPhysics.linearVelocity));
+    }
 
     void applyLift()
     {
         Vector3 normal = transform.forward;
 
-        float projection = 1-Mathf.Abs((Vector3.Dot(normal, Vector3.Normalize(playerPhysics.velocity))));
+        float projection = 1-Mathf.Abs((Vector3.Dot(normal, Vector3.Normalize(playerPhysics.linearVelocity))));
         float planeform = projection * area;
-        float Force = (1f/2f) * fluidDensity * Mathf.Pow(playerPhysics.velocity.magnitude, 2) * planeform;
-        liftDirection =  Vector3.Normalize(Vector3.Cross(transform.right, Vector3.Normalize(playerPhysics.velocity)));
+        float Force = (1f/2f) * fluidDensity * Mathf.Pow(playerPhysics.linearVelocity.magnitude, 2) * planeform;
+        liftDirection =  Vector3.Normalize(Vector3.Cross(transform.right, Vector3.Normalize(playerPhysics.linearVelocity)));
 
 
 
-        Debug.Log(transform.up * Force);
-        playerPhysics.netForce = ( transform.up * Force) + playerPhysics.netForce;
-
+        playerPhysics.AddForce(( liftDirection * Force));
+        applyTorque(Force * liftDirection);
 
     }
 
+    void applyTorque(Vector3 Force)
+    {
 
+        Vector3 r = transform.position - playerPhysics.worldCenterOfMass;
+        playerPhysics.AddTorque(Vector3.Cross(r, Force));
+
+    }
+
+    void applyAngularDrag()
+    {
+        float angcoeff = 10f;
+        playerPhysics.AddTorque( playerPhysics.angularVelocity);
+    }
 }
--- Assets/PhysicsBody.cs
+++ Assets/PhysicsBody.cs
@@ -8,11 +8,15 @@ public class PhysicsBody : MonoBehaviour
     public Vector3 netForce = new Vector3(0, 0, 0);
     public Vector3 velocity = new Vector3(0, 0, 0);
     private Vector3 acceleration = new Vector3(0, 0, 0);
+    private Vector3 torque;
+    private float momentInertia;
+    private Rigidbody rigidBody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.mass = mass;
     }
 
     // Update is called once per frame
@@ -26,22 +30,27 @@ public class PhysicsBody : MonoBehaviour
 
     void LateUpdate(){
 
-        acceleration = (netForce/mass);
-        velocity += acceleration * Time.deltaTime;
-        transform.position += velocity;
 
-        Debug.Log("Velocity "+ Vector3.Magnitude(velocity));
+        Debug.Log("Velocity "+ Vector3.Magnitude(rigidBody.linearVelocity));
         Debug.Log("Force"+ netForce);
         Debug.Log("RealForce" + acceleration* (mass));
         Debug.DrawRay(transform.position, netForce, Color.red);
         Debug.DrawRay(transform.position, velocity, Color.mediumAquamarine);
 
         netForce = new Vector3(0, 0, 0);
+
+
+    }
+
+
+    void ApplyTorque(){
+
+
     }
 
     void ApplyGravity()
     {
-        netForce += new Vector3(0, -0.2f, 0) * mass;
+        rigidBody.AddForce(new Vector3(0, -0.2f, 0) * mass);
 
     }
 }
