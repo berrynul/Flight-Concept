e7270a405f470c6f720ae06eb92ae7e059e5fd54
angel <angel@cyberwings.cyberywings>
2025-12-10 22:20:10 -0800

    Fixed angular velocity drag refactored linear drag
    -Through this the player is actually able to turn and based only on wing
    orientation
    -Lift is still rather broken though

--- Assets/Lift.cs
+++ Assets/Lift.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using MathNet.Numerics;
 
@@ -6,9 +7,14 @@ public class Lift : MonoBehaviour
     private GameObject player;
     private Rigidbody playerPhysics;
     [SerializeField] private float dragCoefficient = 1f;
-    [SerializeField] private float area = 60f;
+    [SerializeField] private float area;
     [SerializeField] private float fluidDensity = 0.01f;
 
+
+    private float height;
+    private float width;
+
+    private double areaIntegrationByR;
     public float dragDirection;
     public Vector3 liftDirection;
 
@@ -16,6 +22,14 @@ public class Lift : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        var renderer = GetComponent<MeshRenderer>();
+        height =renderer.bounds.size.y;
+        width =renderer.bounds.size.x;
+        area = height * width;
+        Func<double, double, double> f = (x,y) => Math.Pow((x*x + y*y), 3d/2d);
+        areaIntegrationByR = Integrate.OnRectangle(f, - width/2f, width/2f, -height/2f, height/2f);
+
+
 
         player = GameObject.Find("Player");
         playerPhysics = player.GetComponent<Rigidbody>();
@@ -25,7 +39,8 @@ public class Lift : MonoBehaviour
     void Update()
     {
         applyDrag();
-        applyLift();
+        //applyLift();
+        applyAngularDrag();
 
     }
 
@@ -44,20 +59,10 @@ public class Lift : MonoBehaviour
 
         Vector3 normal = transform.forward;
         float projection = Vector3.Dot(normal, playerPhysics.linearVelocity.normalized);
-        float Force = (0.5f * Mathf.Pow(Vector3.Magnitude(playerPhysics.linearVelocity), 2) * area * fluidDensity * dragCoefficient);
-        if(projection > 0){
+        Vector3 Force = -(0.5f * playerPhysics.linearVelocity * playerPhysics.linearVelocity.magnitude * area * projection * fluidDensity * dragCoefficient);
 
-            dragDirection = -1;
-
-        }else if(projection < 0){
-            dragDirection = 1;
-
-        }else{
-            dragDirection = 0;
-        }
-
-        playerPhysics.AddForce(dragDirection * projection * Force * Vector3.Normalize(playerPhysics.linearVelocity));
-        applyTorque(dragDirection * projection * Force * Vector3.Normalize(playerPhysics.linearVelocity));
+        playerPhysics.AddForce(Force);
+        applyTorque(Force);
     }
 
     void applyLift()
@@ -87,6 +92,7 @@ public class Lift : MonoBehaviour
     void applyAngularDrag()
     {
         float angcoeff = 10f;
-        playerPhysics.AddTorque( playerPhysics.angularVelocity);
+        Vector3 angularDrag = -(1f/2f)*dragCoefficient * (float)areaIntegrationByR * fluidDensity * playerPhysics.angularVelocity * playerPhysics.angularVelocity.magnitude;
+        playerPhysics.AddTorque(angularDrag);
     }
 }
